7103657c52aaf9b0c6093353d508dfc43e7eea4f
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-21 16:16:13 +0000

    Statement prints after a single withdrawal tested

--- bank/account.py
+++ bank/account.py
@@ -1,4 +1,3 @@
-
 class Account:
     def __init__(self):
         self.transactions = []
--- bank/statement.test.py
+++ bank/statement.test.py
@@ -14,5 +14,17 @@ class TestStatement(unittest.TestCase):
         expected_output = "date || credit || debit || balance\n01/01/2023 || 1.00 || || 1.00"
         self.assertEqual(statement.print_statement(), expected_output)
 
+    def test_prints_after_single_withdrawal(self):
+        transaction1 = Mock()
+        transaction1.format_date.return_value = "01/01/2023"
+        transaction1.format_amount.return_value = "10.00 ||"
+        transaction2 = Mock()
+        transaction2.format_date.return_value = "01/01/2023"
+        transaction2.format_amount.return_value = "|| 1.00"
+        account = Mock(get_transactions=lambda: [transaction1, transaction2], balance_at=lambda i: 10 if i == 0 else 9)
+        statement = Statement(account)
+        expected_output = "date || credit || debit || balance\n01/01/2023 || || 1.00 || 9.00\n01/01/2023 || 10.00 || || 10.00"
+        self.assertEqual(statement.print_statement(), expected_output)
+
 if __name__ == '__main__':
     unittest.main()
