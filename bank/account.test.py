f8a046717d3594cc2c23d68b8262ae1767196917
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-21 14:47:46 +0000

    Multiple account withdrawals/deposits tested

--- bank/account.test.py
+++ bank/account.test.py
@@ -9,11 +9,42 @@ class TestAccount(unittest.TestCase):
         account = Account()
         self.assertTrue(isinstance(account, Account))
 
-    def test_deposit_balance(self):
+    def test_positive_balance_on_deposit(self):
         account = Account()
         transaction = Mock(amount=1, date='01/01/2023')
         account.add_transaction(transaction)
         self.assertEqual(account.latest_balance(), 1.00)
+
+    def test_balance_reduced_on_withdrawal(self):
+        account = Account()
+        transaction1 = Mock(amount=10, date='01/01/2023')
+        transaction2 = Mock(amount=-1, date='01/01/2023')
+        account.add_transaction(transaction1)
+        account.add_transaction(transaction2)
+        self.assertEqual(account.latest_balance(), 9.00)
+
+    def test_positive_balance_multiple_deposits(self):
+        account = Account()
+        transaction1 = Mock(amount=1, date='01/01/2023')
+        transaction2 = Mock(amount=2, date='01/01/2023')
+        transaction3 = Mock(amount=9, date='01/01/2023')
+        account.add_transaction(transaction1)
+        account.add_transaction(transaction2)
+        account.add_transaction(transaction3)
+        self.assertEqual(account.latest_balance(), 12.00)
+
+    def test_balance_multiple_withdrawals(self):
+        account = Account()
+        transaction1 = Mock(amount=20, date='01/01/2023')
+        transaction2 = Mock(amount=-2, date='01/01/2023')
+        transaction3 = Mock(amount=-5, date='01/01/2023')
+        transaction4 = Mock(amount=-11, date='01/01/2023')
+        account.add_transaction(transaction1)
+        account.add_transaction(transaction2)
+        account.add_transaction(transaction3)
+        account.add_transaction(transaction4)
+        self.assertEqual(account.latest_balance(), 2.00)
+
 
 
 if __name__ == '__main__':
