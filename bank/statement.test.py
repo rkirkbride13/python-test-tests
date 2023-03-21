24fed9a344d446425d0044d8d4385f10ec3f72f2
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-21 16:25:43 +0000

    Statement prints with example transactions tested

--- bank/statement.test.py
+++ bank/statement.test.py
@@ -50,5 +50,20 @@ class TestStatement(unittest.TestCase):
         expected_output = "date || credit || debit || balance\n01/01/2023 || || 25.00 || 10.00\n01/01/2023 || || 10.00 || 35.00\n01/01/2023 || 45.00 || || 45.00"
         self.assertEqual(statement.print_statement(), expected_output)
 
+    def test_prints_with_example_transactions(self):
+        transaction1 = Mock()
+        transaction1.format_date.return_value = "10/01/2023"
+        transaction1.format_amount.return_value = "1000.00 ||"
+        transaction2 = Mock()
+        transaction2.format_date.return_value = "13/01/2023"
+        transaction2.format_amount.return_value = "2000.00 ||"
+        transaction3 = Mock()
+        transaction3.format_date.return_value = "14/01/2023"
+        transaction3.format_amount.return_value = "|| 500.00"
+        account = Mock(get_transactions=lambda: [transaction1, transaction2, transaction3], balance_at=lambda i: 1000 if i == 0 else 3000 if i == 1 else 2500)
+        statement = Statement(account)
+        expected_output = "date || credit || debit || balance\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00"
+        self.assertEqual(statement.print_statement(), expected_output)
+
 if __name__ == '__main__':
     unittest.main()
