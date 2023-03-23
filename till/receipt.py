b4982f2cbd09380aaa73e26a0938e90636d0e1eb
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-23 14:06:07 +0000

    Single item is printed on receipt

--- till/receipt.py
+++ till/receipt.py
@@ -1,3 +1,10 @@
 class Receipt:
     def __init__(self):
       self.receipt = []
+
+    def add_order(self, order):
+      self.order = order
+
+    def print_receipt(self):
+       items = self.order.list_items()[0]
+       return f"{items[0]:24}{items[1]} x {items[2]}\n"
--- till/receipt.test.py
+++ till/receipt.test.py
@@ -1,6 +1,6 @@
 import unittest
 from receipt import Receipt
-
+from unittest.mock import Mock
 
 class TestReceipt(unittest.TestCase):
     
@@ -8,6 +8,14 @@ class TestReceipt(unittest.TestCase):
         receipt = Receipt()
         self.assertEqual(isinstance(receipt, Receipt), True)
         self.assertEqual(receipt.receipt, [])
+
+    def test_single_item_order_prints(self):
+        order = Mock()
+        order.list_items.return_value = [['Tea', 1, 3.65]]
+        receipt = Receipt()
+        receipt.add_order(order)
+        self.assertEqual(receipt.print_receipt(), "Tea                     1 x 3.65\n")
+
 
 if __name__ == '__main__':
     unittest.main()
