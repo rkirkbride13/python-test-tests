ebf5e6ea5118eb2934245dd827bdf2c3460b66b1
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-23 12:00:59 +0000

    Exception raised if item is not on the menu

--- till/item.py
+++ till/item.py
@@ -3,10 +3,13 @@ from inventory import inventory
 class Item:
 
     def set_name(self, name):
-        self.name = name
+        if name in inventory['prices'][0]:
+            self.name = name
+        else:
+            raise Exception('This item does not exist on the menu')
 
     def get_name(self):
         return self.name
-    
+
     def get_price(self):
         return inventory['prices'][0][self.name]
--- till/item.test.py
+++ till/item.test.py
@@ -2,12 +2,17 @@ import unittest
 from item import Item
 
 class TestItem(unittest.TestCase):
-    def test_init(self):
+    def test_name_and_price(self):
         item = Item()
         item.set_name('Tea')
         self.assertTrue(isinstance(item, Item))
         self.assertEqual(item.get_name(), 'Tea')
         self.assertEqual(item.get_price(),3.65)
+
+    def test_error_raised_if_item_nonexistant(self):
+        item = Item()
+        with self.assertRaises(Exception):
+            item.set_name('Pizza')
 
 if __name__ == '__main__':
     unittest.main()
