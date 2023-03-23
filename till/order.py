c3fc9b5e0c3561e378cdcd695e3d74de7355f405
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-23 12:21:25 +0000

    Item added to order tested

--- till/order.py
+++ till/order.py
@@ -1,9 +1,22 @@
 class Order:
     
+    def __init__(self):
+        self.order = []
+
     def set_name(self, name = '-'):
         self.name = name
 
     def get_name(self):
         return self.name
     
-    
+    def add_item(self, new_item):
+        item_already_ordered = False
+        for item in self.order:
+            if new_item.get_name() in item:
+                item[1] += 1
+                item_already_ordered = True
+        if not item_already_ordered:
+            self.order.append([new_item.get_name(), 1, new_item.get_price()])
+
+    def list_items(self):
+        return self.order
--- till/order.test.py
+++ till/order.test.py
@@ -1,5 +1,6 @@
 import unittest
 from order import Order
+from unittest.mock import Mock
 
 
 class TestOrder(unittest.TestCase):
@@ -9,6 +10,13 @@ class TestOrder(unittest.TestCase):
         order.set_name("Robbie")
         self.assertEqual(order.get_name(), 'Robbie')
 
+    def test_add_item(self):
+        item = Mock()
+        item.get_name.return_value = 'Tea'
+        item.get_price.return_value = 3.65
+        order = Order()
+        order.add_item(item)
+        self.assertEqual(order.list_items(), [['Tea', 1, 3.65]])
 
 if __name__ == '__main__':
     unittest.main()
