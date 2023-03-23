ffdac9bd0d123d081a7bece2ca072cccd254c2d9
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-23 12:24:09 +0000

    Multiple same item added to order

--- till/order.test.py
+++ till/order.test.py
@@ -18,7 +18,7 @@ class TestOrder(unittest.TestCase):
         order.add_item(item)
         self.assertEqual(order.list_items(), [['Tea', 1, 3.65]])
 
-    def test_multiple_items_added(self):
+    def test_multiple_different_items_added(self):
         item = Mock()
         item.get_name.return_value = 'Tea'
         item.get_price.return_value = 3.65
@@ -29,6 +29,15 @@ class TestOrder(unittest.TestCase):
         order.add_item(item)
 
         self.assertEqual(order.list_items(), [['Tea', 1, 3.65], ['Cortado', 1, 4.55]])
+
+    def test_multiple_same_item_added(self):
+        item = Mock()
+        item.get_name.return_value = 'Tea'
+        item.get_price.return_value = 3.65
+        order = Order()
+        order.add_item(item)
+        order.add_item(item)
+        self.assertEqual(order.list_items(), [['Tea', 2, 3.65]])
 
 if __name__ == '__main__':
     unittest.main()
