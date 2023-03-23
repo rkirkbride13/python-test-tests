bd72ae299fdc66d21eb9ceb051634b6632013780
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2023-03-23 14:30:39 +0000

    Bill being printed tested

--- till/receipt.py
+++ till/receipt.py
@@ -5,10 +5,12 @@ class Receipt:
     def add_order(self, order):
       self.order = order
 
-    def print_receipt(self):
+    def print_receipt(self, tax):
        items = self.order.list_items()
        for item in items:
           self.receipt.append(f"{item[0]:24}{item[1]} x {item[2]}\n")
-       tax = f"{'Tax':24}${self.order.calc_tax():.2f}\n"
+       tax = f"{'Tax':24}${self.order.calc_tax(tax):.2f}\n"
        self.receipt.append(tax)
+       total = f"{'Total':24}${self.order.calc_bill(tax):.2f}\n"
+       self.receipt.append(total)
        return ''.join(self.receipt)
--- till/receipt.test.py
+++ till/receipt.test.py
@@ -12,38 +12,46 @@ class TestReceipt(unittest.TestCase):
     def test_single_item_order_prints(self):
         order = Mock()
         order.list_items.return_value = [['Tea', 1, 3.65]]
-        order.item_total.return_value = 3.65
         order.calc_tax.return_value = 0.30
+        order.calc_bill.return_value = 3.95
         receipt = Receipt()
         receipt.add_order(order)
-        self.assertIn("Tea                     1 x 3.65\n", receipt.print_receipt())
+        self.assertIn("Tea                     1 x 3.65\n", receipt.print_receipt(8.5))
 
     def test_varied_items_order_prints(self):
         order = Mock()
         order.list_items.return_value = [['Tea', 1, 3.65], ['Cortado', 1, 4.55]]
-        order.item_total.return_value = 8.20
         order.calc_tax.return_value = 0.70
+        order.calc_bill.return_value = 8.90
         receipt = Receipt()
         receipt.add_order(order)
-        self.assertIn("Tea                     1 x 3.65\nCortado                 1 x 4.55\n", receipt.print_receipt())
+        self.assertIn("Tea                     1 x 3.65\nCortado                 1 x 4.55\n", receipt.print_receipt(8.5))
 
     def test_multi_item_order_prints(self):
         order = Mock()
         order.list_items.return_value = [['Tea', 2, 3.65]]
-        order.item_total.return_value = 7.30
         order.calc_tax.return_value = 0.60
+        order.calc_bill.return_value = 7.90
         receipt = Receipt()
         receipt.add_order(order)
-        self.assertIn("Tea                     2 x 3.65\n", receipt.print_receipt())
+        self.assertIn("Tea                     2 x 3.65\n", receipt.print_receipt(8.5))
 
     def test_tax_prints(self):
         order = Mock()
         order.list_items.return_value = [['Tea', 1, 3.65]]
-        order.item_total.return_value = 3.65
         order.calc_tax.return_value = 0.30
+        order.calc_bill.return_value = 3.95
         receipt = Receipt()
         receipt.add_order(order)
-        self.assertIn("Tea                     1 x 3.65\nTax                     $0.30\n", receipt.print_receipt())
+        self.assertIn("Tax                     $0.30\n", receipt.print_receipt(8.5))
 
+    def test_item_total_prints(self):
+        order = Mock()
+        order.list_items.return_value = [['Tea', 1, 3.65], ['Cortado', 1, 4.55]]
+        order.calc_tax.return_value = 0.70
+        order.calc_bill.return_value = 8.90
+        receipt = Receipt()
+        receipt.add_order(order)
+        self.assertIn("Total                   $8.90\n", receipt.print_receipt(8.5))
 if __name__ == '__main__':
     unittest.main()
